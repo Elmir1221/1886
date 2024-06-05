31e6dcf1f9758fe2038dc63ce49bdf9ee0d4fcda
Gafarzada Elmir <elmirmg@code.local>
2024-06-05 08:56:29 +0400

    task

---
+++ New folder (9)/New folder (7)/home_project/home_project/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace home_project.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult index()
+        { 
+            return View();
+        }
+    }
+}
